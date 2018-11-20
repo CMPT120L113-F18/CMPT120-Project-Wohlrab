f3332bce3906278a1ec634764facbb30d551004f
Ashley Wohlrab <Ashley.Wohlrab1@marist.edu>
2018-11-20 16:11:05 -0500

    Add four items to your game.

--- game.py
+++ game.py
@@ -30,18 +30,27 @@ def GameStart():
         input(introductionMessage)
 
 #store locales in list
-locales=["You are standing in the empty control room. There is an air duct on the south wall, doors on the east and west walls, and strange looking tool laying on the desk.",
-         "Going through the North door, you find yourself back in the hallway you came from.",
-         "By going through the West door, you arrive in a small armory. Surrounding you are numerous weapons and shields.",
-         "Using the tool, you pry the vent from the duct. It's dark inside, but you can just make out the path in front of you. There is another vent straight ahead.",
-         "You use the tool to pry open the vent, and find yourself inside a storage closet containing Storm Trooper uniforms and a single exit door on the south wall.",
-         "Through the door you find a hallway with Storm Troopers heading East down the hall. There is another corridor in the Eastern direction of the hallway.",
+locales=["You are standing in the empty control room. There is an air duct on the south wall, doors on the east and west walls, and a rolled up scroll laying on the desk.",
+         "You find yourself back in the hallway you exited before.",
+         "You arrive in a small armory. Surrounding you are numerous weapons and shields. There's a strange looking tool on the floor ahead of you.",
+         "You pry the vent from the duct. It's dark inside, but you can just make out the path in front of you. There is another vent straight ahead.",
+         "You pry open the vent, and find yourself inside a storage closet containing Storm Trooper uniforms and a single exit door on the south wall.",
+         "You find a hallway with Storm Troopers heading East down the hall. There is another corridor in the Eastern direction of the hallway.",
          "You find yourself inside a food storage room. The walls are lined with cans and snacks.",
-         "Following the crowd, you find your way to the ship's vehicle hangar. There are Rebels and Storm-troopers fighting closeby, and an empty tie fighter on the east wall.",
+         "You find your way to the ship's vehicle hangar. There are Rebels and Storm-troopers fighting closeby, and an empty tie fighter on the east wall.",
          "The room is filled with medical tools and has a large medical table in the center. There are med-paks lying on a nearby table.",
          "There are books lining the walls. You notice a leather-bound book on a nearby shelf. It seems to have been recently opened."]
 
-locNames=["Control Room", "North Hallway", "Armory", "Air Duct", "Uniform Closet", "South Hallway", "Food Closet", "Hangar", "Medical Bay", "Library"] 
+locNames=[["Control Room", "Map"],
+          ["North Hallway", None],
+          ["Armory", "Tool"],
+          ["Air Duct", None],
+          ["Uniform Closet", None],
+          ["South Hallway", None],
+          ["Food Closet", "Can"],
+          ["Hangar", None],
+          ["Medical Bay", "Med-pak"],
+          ["Library", "Book"]] 
 
 hasBeen=[False, False, False, False, False, False, False, False, False, False]
 
@@ -72,6 +81,9 @@ def printCount():
         #global count
         player["Moves Made"] = player["Moves Made"] + 1
         print("Moves made: " + str(player["Moves Made"]))
+
+def printItem(playerLocation):
+        print("Item located at this location: " + str(locNames[playerLocation][1]) + ".")
 
 def printMap():
         print("        ----------------------------------------")
@@ -118,6 +130,7 @@ def playerUpdate(playerLocation):
         printLocation()
         printScore(playerLocation)
         printCount()
+        printItem(playerLocation)
         
 
 North=""
@@ -137,9 +150,11 @@ def ControlRoom():
         West = "Armory"
         global South
         South = "Air Duct"
-        player["Player Location"] = locNames[0]
+        player["Player Location"] = locNames[0][0]
+        item = locNames[0][1]
         playerUpdate(0)
         print(locales[0])
+        
 
 def NorthHallway():
         global North
@@ -150,9 +165,10 @@ def NorthHallway():
         West = ""
         global South
         South = "Control Room"
-        player["Player Location"] = locNames[1]
+        player["Player Location"] = locNames[1][0]
         playerUpdate(1)
         print(locales[1])
+       
 
 def Armory():
         global North
@@ -163,9 +179,10 @@ def Armory():
         West = ""
         global South
         South = ""
-        player["Player Location"] = locNames[2]
+        player["Player Location"] = locNames[2][0]
         playerUpdate(2)
         print(locales[2])
+
 
 
 def AirDuct():
@@ -177,9 +194,10 @@ def AirDuct():
         West = ""
         global South
         South = "Uniform Closet"
-        player["Player Location"] = locNames[3]
+        player["Player Location"] = locNames[3][0]
         playerUpdate(3)
         print(locales[3])
+
 
 def UniformCloset():
         global North
@@ -190,9 +208,10 @@ def UniformCloset():
         West = ""
         global South
         South = "South Hallway"
-        player["Player Location"] = locNames[4]
+        player["Player Location"] = locNames[4][0]
         playerUpdate(4)
         print(locales[4])
+
 
 def SouthHallway():
         global North
@@ -203,7 +222,7 @@ def SouthHallway():
         West = "Food Closet"
         global South
         South = ""
-        player["Player Location"] = locNames[5]
+        player["Player Location"] = locNames[5][0]
         playerUpdate(5)
         print(locales[5])
 
@@ -216,9 +235,10 @@ def FoodCloset():
         West = ""
         global South
         South = ""
-        player["Player Location"] = locNames[6]
+        player["Player Location"] = locNames[6][0]
         playerUpdate(6)
         print(locales[6])
+
 
 def Hangar():
         global North
@@ -229,9 +249,10 @@ def Hangar():
         West = "South Hallway"
         global South
         South = ""
-        player["Player Location"] = locNames[7]
+        player["Player Location"] = locNames[7][0]
         playerUpdate(7)
         print(locales[7])
+
 
 def MedicalBay():
         global North
@@ -242,9 +263,10 @@ def MedicalBay():
         West = "North Hallway"
         global South
         South = ""
-        player["Player Location"] = locNames[8]
+        player["Player Location"] = locNames[8][0]
         playerUpdate(8)
         print(locales[8])
+
 
 def Library():
         global North
@@ -255,9 +277,10 @@ def Library():
         West = "Control Room"
         global South
         South = ""
-        player["Player Location"] = locNames[9]
+        player["Player Location"] = locNames[9][0]
         playerUpdate(9)
         print(locales[9])
+
 
 def playGame():
         Game = True
@@ -319,10 +342,11 @@ def playGame():
                         break
                 elif input == "map":
                         printMap()
+                        
                 else:
                               print("The command you have entered is invalid.")
                               continue
-                if player["Moves Made"] == 15:
+                if player["Moves Made"] == 18:
                         print("You have been caught! Game Over!")
                         break
                 else:
@@ -338,6 +362,8 @@ player = {
         "Moves Made": -1
         }
 
+
+              
 #end the game and show credits
 def EndGame():
         print("Getting into the tie fighter, you turn the key and start the engine. Flying away from the vessel, you look out into space and feel hopeful for the future of the galaxy. Game over!")
